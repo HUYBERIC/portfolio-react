e230947a34d41e80eeec23e662989956929b10ec
huyberic <huyberic@gmail.com>
2024-12-27 13:57:24 +0100

    education update

--- src/components/Education.jsx
+++ src/components/Education.jsx
@@ -6,14 +6,14 @@ const EducationPage = () => {
     <section className="first-section-education" id="education">
       <h2 className="heading">Education</h2>
 
-      <div className="timeline-items">
         <div className="timeline-item">
           <div className="timeline-dot"></div>
-          <div className="timeline-date">2014</div>
+          <div className="timeline-date">2025</div>
           <div className="timeline-content">
-            <h3>Secondary Education</h3>
+            <h3>Professional Training - Career Change</h3>
             <p>
-              Graduated in General Studies with a focus on Economic Sciences.
+              Fullstack Web Developer training with a 2-month internship at
+              Liège Airport.
             </p>
           </div>
         </div>
@@ -30,17 +30,18 @@ const EducationPage = () => {
           </div>
         </div>
 
+        <div className="timeline-items">
         <div className="timeline-item">
           <div className="timeline-dot"></div>
-          <div className="timeline-date">2025</div>
+          <div className="timeline-date">2014</div>
           <div className="timeline-content">
-            <h3>Professional Training - Career Change</h3>
+            <h3>Secondary Education</h3>
             <p>
-              Fullstack Web Developer training with a 2-month internship at
-              Liège Airport.
+              Graduated in General Studies with a focus on Economic Sciences.
             </p>
           </div>
         </div>
+        
       </div>
     </section>
   );
